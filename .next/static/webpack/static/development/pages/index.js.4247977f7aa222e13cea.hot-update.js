9da8ed6f0ecf0e3c70f9899f41ff344c37154f59
Lac Tran <trangialac.fin@gmail.com>
2019-10-22 12:42:04 +0300

    first commit

---
+++ .next/static/webpack/static/development/pages/index.js.4247977f7aa222e13cea.hot-update.js
@@ -0,0 +1,17 @@
+webpackHotUpdate("static\\development\\pages\\index.js",[
+/* 0 */,
+/* 1 */
+false,
+/* 2 */
+/*!**************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F&absolutePagePath=D%3A%5Cself-study%5CNextJS%5Cpages%5Cindex.js ***!
+  \**************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F&absolutePagePath=D%3A%5Cself-study%5CNextJS%5Cpages%5Cindex.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=D%3A%5Cself-study%5CNextJS%5Cpages%5Cindex.js!./");
+
+
+/***/ })
+])
+//# sourceMappingURL=index.js.4247977f7aa222e13cea.hot-update.js.map
